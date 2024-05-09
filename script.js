c26012467b88af2553b42ce9f9296ffb84d32065
Classic_User <supriyapkadu@gmail.com>
2024-05-09 18:10:40 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//function for addition of two numbers 
+/*function addNumber(no1, no2) {
+    // no3 = no1 + no2;
+    // document.write("Addition of two numbers:" + no3);
+    return no1 + no2;
+
+}
+let x = addNumber(10, 20);
+document.write("Addition of two numbers:" + x);*/
